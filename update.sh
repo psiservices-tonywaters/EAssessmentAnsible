e0f8b443c236eada620e6bac2deade074abc5501
Tony Waters <tw_ld@npd.learndirect.com>
2019-01-29 16:11:56 +0000

    Initial commit

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ oc login https://ocp.88-160-14-7bdf86.frn00006.cna.ukcloud.com:8443 --token=lK91
 oc project st-1
 oc get pods -o jsonpath='active_services:{range .items[*]}{"\n- "}name: {.metadata.name}{"\n  "}value: {.status.podIP}{end}' > vars/services_running.yml
 
-cat services_running.yml
+cat vars/services_running.yml
 
 whoami && pwd && ls -la
 
