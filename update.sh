9b655279e55550645ab4e5211db124bcebc08f30
Tony Waters <tw_ld@npd.learndirect.com>
2019-01-29 14:21:04 +0000

    Initial commit

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env /bin/sh
 
 wget --user='admin' --password='admin123' 'http://nexus-nexus.88-160-14-7bdf86.frn00006.cna.ukcloud.com/repository/utils/oc'
 oc version
