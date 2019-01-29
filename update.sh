9ebb9888c011046c8f87353b43cf15b11c127e54
Tony Waters <tw_ld@npd.learndirect.com>
2019-01-29 15:42:09 +0000

    Initial commit

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ oc version
 oc login https://ocp.88-160-14-7bdf86.frn00006.cna.ukcloud.com:8443 --token=lK91HtlajPCNqXBxHp0bULjGTgi8nbJptFxt7I86XQA
 
 oc project st-1
-oc get pods -o jsonpath='{range .items[*]}{.metadata.name}:{.status.podIP}{"\n"}{end}' > data
+oc get pods -o jsonpath='services:{range .items[*]}{"\n- "}name: {.metadata.name}{"\n  "}value: {.status.podIP}{end}' > data
 
 cat data
 
