8d8026e768e9657788db00d9db71fa8ad2eac9a5
Tony Waters <tw_ld@npd.learndirect.com>
2019-01-29 15:16:13 +0000

    Initial commit

--- update.sh
+++ update.sh
@@ -5,7 +5,13 @@ oc version
 
 oc login https://ocp.88-160-14-7bdf86.frn00006.cna.ukcloud.com:8443 --token=lK91HtlajPCNqXBxHp0bULjGTgi8nbJptFxt7I86XQA
 
-oc get pods -o jsonpath='{range .items[*]}{.metadata.name}:{.status.podIP}{"\n"}{end}'
+oc project st-1
+oc get pods -o jsonpath='{range .items[*]}{.metadata.name}:{.status.podIP}{"\n"}{end}' > data
+
+cat data
+
+ansible-playbook playbook.yml
+
 
 
 
