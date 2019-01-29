3e8fcba278a81c7d6ab3a2411e40e94d5d70153b
Tony Waters <tw_ld@npd.learndirect.com>
2019-01-29 14:55:23 +0000

    Initial commit

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env /bin/sh
 
-wget --user='admin' --password='admin123' 'http://nexus-nexus.88-160-14-7bdf86.frn00006.cna.ukcloud.com/repository/utils/oc'
+echo "Running OC binary"
 oc version
+
+oc login https://ocp.88-160-14-7bdf86.frn00006.cna.ukcloud.com:8443 --token=lK91HtlajPCNqXBxHp0bULjGTgi8nbJptFxt7I86XQA
+
+oc get pods -o jsonpath='{range .items[*]}{.metadata.name}:{.status.podIP}{"\n"}{end}'
+
 
 
